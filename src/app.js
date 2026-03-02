9a084a43d0ab8dd4e3ba6b3e9a0351675236a2b4
JoselynFio <fiorecortes47@gmail.com>
2026-03-02 12:12:06 +0100

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function whoToBlame(){
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 
+  const randomIndex = Math.floor (Math.random() * who.length);
+ return who[randomIndex]
+}
+
+function actionTheyDid(){
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  const randomIndex = Math.floor( Math.random() * action.length);
+  return action[randomIndex]
+}
+function whatToAvoid(){
+ let what = ['my homework', 'my phone', 'the car'];
+ const randomIndex = Math.floor( Math.random() * what.length);
+  return what[randomIndex]
+}
+function when(){
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+const randomIndex = Math.floor( Math.random() * when.length);
+  return when[randomIndex]
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const h2Element = document.getElementById('excuse')
+  
+  h2Element.innerHTML =  `${whoToBlame()} ${actionTheyDid()} ${whatToAvoid()} ${when()}`
 };
